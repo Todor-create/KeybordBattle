dc04b7e6ddfa2e09f6a7b16079b32e9744c3f7ce
Todor-create <as_project@abv.bg>
2020-10-20 17:49:01 +0300

    preload and shortening asset names
    assets added to  the preload scene and Animation is shortened to Anim

--- src/scene/Preload.ts
+++ src/scene/Preload.ts
@@ -11,6 +11,17 @@ class Preload extends Phaser.Scene {
         this.load.on("load", this.onFileLoaded, this);
         this.load.on("complete", this.onComplete, this);
 
+        this.load.atlas("balistaAnim", "./assets/images/balistaAnim.png", "./assets/images/balistaAnim.json");
+        this.load.atlas("cannonAnim", "./assets/images/cannonAnim.png", "./assets/images/cannonAnim.json");
+        this.load.atlas("ogers", "./assets/images/ogers.png", "./assets/images/ogers.json");
+
+        this.load.image("128x64", "./assets/images/128x64.png");
+        this.load.image("desert128x64", "./assets/images/desert128x64.png");
+        this.load.image("grass128x64", "./assets/images/grass128x64.png");
+        this.load.image("buildingIcon", "./assets/images/buildingIcon.png");
+        this.load.image("pointer", "./assets/images/pointer.png");
+        this.load.tilemapTiledJSON("testMap", "./assets/images/testMap.json");
+        
         this.load.start();
     }
 
